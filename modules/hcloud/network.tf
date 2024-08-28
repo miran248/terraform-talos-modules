4d330b067056ed98f9449464216aa4bb3df935c1
miran248 <miran@248.sh>
2024-08-28 13:17:30 +0200

    fix(hcloud): add back unused subnets

--- modules/hcloud/network.tf
+++ modules/hcloud/network.tf
@@ -10,15 +10,15 @@ resource "hcloud_network_subnet" "machines" {
   ip_range     = var.layout.cidrs4.machines
   type         = "cloud"
 }
-# resource "hcloud_network_subnet" "services" {
-#   network_id   = hcloud_network.this.id
-#   network_zone = var.location.network_zone
-#   ip_range     = var.layout.cidrs4.services
-#   type         = "cloud"
-# }
-# resource "hcloud_network_subnet" "pods" {
-#   network_id   = hcloud_network.this.id
-#   network_zone = var.location.network_zone
-#   ip_range     = var.layout.cidrs4.pods
-#   type         = "cloud"
-# }
+resource "hcloud_network_subnet" "services" {
+  network_id   = hcloud_network.this.id
+  network_zone = var.location.network_zone
+  ip_range     = var.layout.cidrs4.services
+  type         = "cloud"
+}
+resource "hcloud_network_subnet" "pods" {
+  network_id   = hcloud_network.this.id
+  network_zone = var.location.network_zone
+  ip_range     = var.layout.cidrs4.pods
+  type         = "cloud"
+}
