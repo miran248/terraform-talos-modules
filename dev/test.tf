e5f935cf62502c12ce50fb3a3a264eb32aa370f6
miran248 <miran@248.sh>
2024-10-23 14:01:49 +0200

    chore: turn off cilium authn

--- dev/test.tf
+++ dev/test.tf
@@ -1,46 +1,46 @@
-data "hcloud_image" "dev1_v1_8_1_amd64" {
+data "hcloud_image" "dev3_v1_8_1_amd64" {
   with_selector = "name=talos,version=v1.8.1,arch=amd64"
 }
-data "hcloud_datacenter" "dev1_nuremberg" {
+data "hcloud_datacenter" "dev3_nuremberg" {
   name = "nbg1-dc3"
 }
-data "hcloud_datacenter" "dev1_helsinki" {
+data "hcloud_datacenter" "dev3_helsinki" {
   name = "hel1-dc2"
 }
 
 locals {
-  dev1_patches_zitadel = <<-EOF
+  dev3_patches_zitadel = <<-EOF
     machine:
       nodeLabels:
         app: zitadel
   EOF
 
-  image_id = data.hcloud_image.dev1_v1_8_1_amd64.id
+  image_id = data.hcloud_image.dev3_v1_8_1_amd64.id
 }
 
-module "dev1_nuremberg_pool" {
+module "dev3_nuremberg_pool" {
   source = "../modules/hcloud-pool"
 
-  prefix     = "dev1-nbg"
-  datacenter = data.hcloud_datacenter.dev1_nuremberg.name
+  prefix     = "dev3-nbg"
+  datacenter = data.hcloud_datacenter.dev3_nuremberg.name
 
   control_planes = [
     { server_type = "cx22", image_id = local.image_id },
-    { server_type = "cx22", image_id = local.image_id },
-    { server_type = "cx22", image_id = local.image_id },
+    # { server_type = "cx22", image_id = local.image_id },
+    # { server_type = "cx22", image_id = local.image_id },
   ]
   workers = [
-    # { server_type = "cx22", image_id = local.image_id, patches = [local.dev1_patches_zitadel] },
-    # { server_type = "cx22", image_id = local.image_id, patches = [local.dev1_patches_zitadel] },
-    # { server_type = "cx22", image_id = local.image_id, patches = [local.dev1_patches_zitadel] },
+    # { server_type = "cx22", image_id = local.image_id, patches = [local.dev3_patches_zitadel] },
+    # { server_type = "cx22", image_id = local.image_id, patches = [local.dev3_patches_zitadel] },
+    # { server_type = "cx22", image_id = local.image_id, patches = [local.dev3_patches_zitadel] },
   ]
 }
 
-module "dev1_helsinki_pool" {
+module "dev3_helsinki_pool" {
   source = "../modules/hcloud-pool"
 
-  prefix     = "dev1-hel"
-  datacenter = data.hcloud_datacenter.dev1_helsinki.name
+  prefix     = "dev3-hel"
+  datacenter = data.hcloud_datacenter.dev3_helsinki.name
 
   workers = [
     { server_type = "cx22", image_id = local.image_id },
@@ -48,17 +48,17 @@ module "dev1_helsinki_pool" {
   ]
 }
 
-module "dev1_talos_cluster" {
+module "dev3_talos_cluster" {
   source = "../modules/talos-cluster"
 
-  name               = "dev1"
-  endpoint           = "dev1.dev.248.sh"
+  name               = "dev3"
+  endpoint           = "dev3.dev.248.sh"
   talos_version      = "v1.8.1"
   kubernetes_version = "v1.31.1"
 
   pools = [
-    module.dev1_nuremberg_pool,
-    module.dev1_helsinki_pool,
+    module.dev3_nuremberg_pool,
+    module.dev3_helsinki_pool,
   ]
 
   patches = {
@@ -125,34 +125,34 @@ module "dev1_talos_cluster" {
   }
 }
 
-module "dev1_hcloud_apply" {
+module "dev3_hcloud_apply" {
   for_each = merge([for pool in [
-    module.dev1_nuremberg_pool,
-    module.dev1_helsinki_pool,
+    module.dev3_nuremberg_pool,
+    module.dev3_helsinki_pool,
   ] : { "${pool.prefix}" = pool }]...)
   source = "../modules/hcloud-apply"
 
-  cluster = module.dev1_talos_cluster
+  cluster = module.dev3_talos_cluster
   pool    = each.value
 }
 
-module "dev1_talos_apply" {
+module "dev3_talos_apply" {
   source = "../modules/talos-apply"
 
-  cluster = module.dev1_talos_cluster
+  cluster = module.dev3_talos_cluster
 }
 
-resource "google_dns_record_set" "dev1_talos_ipv6" {
-  name         = "${module.dev1_talos_cluster.name}.${data.google_dns_managed_zone.this.dns_name}"
+resource "google_dns_record_set" "dev3_talos_ipv6" {
+  name         = "${module.dev3_talos_cluster.name}.${data.google_dns_managed_zone.this.dns_name}"
   managed_zone = data.google_dns_managed_zone.this.name
   type         = "AAAA"
   ttl          = 300
 
-  rrdatas = module.dev1_talos_cluster.public_ips6.control_planes
+  rrdatas = module.dev3_talos_cluster.public_ips6.control_planes
 }
 
 # outputs
 output "talos_config" {
-  value     = module.dev1_talos_cluster.talos_config
+  value     = module.dev3_talos_cluster.talos_config
   sensitive = true
 }
