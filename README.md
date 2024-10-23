2ea8f730c89c4bb50c8dd2a3d2e5de824837c28e
miran248 <miran@248.sh>
2024-10-23 12:37:55 +0200

    docs: update README.md and examples

--- README.md
+++ README.md
@@ -1,39 +1,47 @@
 # terraform-talos-modules
-This repository contains a collection of opinionated terraform modules for running [talos](https://www.talos.dev) on [hetzner](https://www.hetzner.com).
+Modules in this repository help provision and maintain multi-region [kubernetes](https://kubernetes.io) clusters on [hetzner](https://www.hetzner.com).
 
 ## features
-- talos with kubespan, kubeprism and hostdns
-- ipv6 and/or ipv4 connectivity
-- hcloud ipv4 private network(s)
-- dual stack internals
-- [cilium](https://cilium.io)
-- [talos-cloud-controller-manager](https://github.com/siderolabs/talos-cloud-controller-manager)
+- [talos](https://www.talos.dev) with kubeprism and hostdns, without kubespan
+- ipv6-only connectivity
+- single-stack, ipv6 internals
+- [cilium](https://cilium.io) - direct routing (veth), bigtcp, bbr and pod-to-pod wireguard encryption
+- [talos-ccm](https://github.com/siderolabs/talos-cloud-controller-manager) - certificates and ipam (CloudAllocator)
+- [hcloud-ccm](https://github.com/hetznercloud/hcloud-cloud-controller-manager) - auto-provisioned load balancers
+- [hcloud-csi](https://github.com/hetznercloud/csi-driver) - storage
 
 ## modules
-1. [talos-cluster](modules/talos-cluster) handles cluster-wide patches, including cni and ccm
-2. [node-pool](modules/node-pool) handles machine-specific patches, defines machines and private ips
-3. [hcloud-network](modules/hcloud-network) adds hetzner-specific patches, manages hcloud networks, firewalls and public ips
-4. [talos-config](modules/talos-config) manages talos configs for all machines
-5. [hcloud-servers](modules/hcloud-servers) manages hcloud servers and firewalls
-6. [talos-apply](modules/talos-apply) bootstraps cluster and applies configs
+1. [hcloud-pool](modules/hcloud-pool) allocates public ipv6 /64 blocks for all defined nodes
+2. [talos-cluster](modules/talos-cluster) forms a talos cluster from previously defined node pools
+3. [hcloud-apply](modules/hcloud-apply) provisions servers with talos configs and sets up firewalls
+4. [talos-apply](modules/talos-apply) bootstraps the cluster and handles all future config changes
 
 ## examples
 See [examples](examples) folder.
+
+## try it out
+1. clone the repo
+2. navigate to [dev](dev) folder and run [just](https://github.com/casey/just) to deploy the cluster
+3. navigate to [manifests](manifests) folder and run `just` to generate all necessary yaml files
+4. apply them individually, `talos-ccm` and `cilium` are required
 
 ## diagram
 The following [mermaid](https://github.com/mermaid-js/mermaid) flowchart outlines the order of operations between different modules for a cluster, spanning two regions.
 
 ```mermaid
 %%{init: {'theme': 'neutral' } }%%
-flowchart TD
-    talos-cluster --> node-pool-nuremberg-1
-    talos-cluster --> node-pool-helsinki-1
-    node-pool-nuremberg-1[node-pool nbg 1] --> hcloud-network-nuremberg-1
-    node-pool-helsinki-1[node-pool hel 1] --> hcloud-network-helsinki-1
-    hcloud-network-nuremberg-1[hcloud-network nbg 1] --> talos-config
-    hcloud-network-helsinki-1[hcloud-network hel 1] --> talos-config
-    talos-config --> hcloud-servers-nuremberg-1
-    talos-config --> hcloud-servers-helsinki-1
-    hcloud-servers-nuremberg-1[hcloud-servers nbg 1] --> talos-apply
-    hcloud-servers-helsinki-1[hcloud-servers hel 1] --> talos-apply
+graph TD
+    A[/hcloud-pool nbg/]
+    B[/hcloud-pool hel/]
+    C[talos-cluster]
+    D[hcloud-apply nbg]
+    E[hcloud-apply hel]
+    F(((talos-apply)))
+    A --> C
+    A --> D
+    B --> C
+    C --> D
+    C --> F
+    C --> E
+    B --> E
 ```
---
+++ examples/multi-region.tf
@@ -0,0 +1,138 @@
+data "hcloud_image" "v1_8_1_amd64" {
+  with_selector = "name=talos,version=v1.8.1,arch=amd64"
+}
+data "hcloud_datacenter" "nuremberg" {
+  name = "nbg1-dc3"
+}
+data "hcloud_datacenter" "helsinki" {
+  name = "hel1-dc2"
+}
+
+locals {
+  patches_zitadel = <<-EOF
+    machine:
+      nodeLabels:
+        app: zitadel
+  EOF
+
+  image_id = data.hcloud_image.v1_8_1_amd64.id
+}
+
+module "nuremberg_pool" {
+  source = "github.com/miran248/terraform-talos-modules//modules/hcloud-pool?ref=v2.0.0"
+
+  prefix     = "nbg"
+  datacenter = data.hcloud_datacenter.nuremberg.name
+
+  control_planes = [
+    { server_type = "cx22", image_id = local.image_id },
+    { server_type = "cx22", image_id = local.image_id },
+    { server_type = "cx22", image_id = local.image_id },
+  ]
+  workers = [
+    { server_type = "cx22", image_id = local.image_id, patches = [local.patches_zitadel] },
+    # { server_type = "cx22", image_id = local.image_id, patches = [local.patches_zitadel] },
+    # { server_type = "cx22", image_id = local.image_id, patches = [local.patches_zitadel] },
+  ]
+}
+
+module "helsinki_pool" {
+  source = "github.com/miran248/terraform-talos-modules//modules/hcloud-pool?ref=v2.0.0"
+
+  prefix     = "hel"
+  datacenter = data.hcloud_datacenter.helsinki.name
+
+  workers = [
+    { server_type = "cx22", image_id = local.image_id },
+    # { server_type = "cx22", image_id = local.image_id },
+  ]
+}
+
+module "talos_cluster" {
+  source = "github.com/miran248/terraform-talos-modules//modules/talos-cluster?ref=v2.0.0"
+
+  name               = "example"
+  endpoint           = "k.example.com"
+  talos_version      = "v1.8.1"
+  kubernetes_version = "v1.31.1"
+
+  pools = [
+    module.nuremberg_pool,
+    module.helsinki_pool,
+  ]
+
+  patches = {
+    common = [
+      yamlencode({
+        cluster = {
+          network = {
+            cni = {
+              name = "none"
+            }
+          }
+          inlineManifests = [
+            {
+              name     = "hcloud-secret",
+              contents = <<-EOF
+                apiVersion: v1
+                kind: Secret
+                metadata:
+                  name: hcloud
+                  namespace: kube-system
+                stringData:
+                  token: ${var.hcloud_token}
+                type: Opaque
+              EOF
+            },
+          ]
+        }
+        machine = {
+          network = {
+            nameservers = [
+              "2a00:1098:2b::1",      # https://nat64.net
+              "2a00:1098:2c::1",      # https://nat64.net
+              "2a01:4f8:c2c:123f::1", # https://nat64.net
+            ]
+          }
+          time = {
+            servers = [
+              "/dev/ptp0",
+            ]
+          }
+        }
+      }),
+    ]
+  }
+}
+
+module "hcloud_apply" {
+  for_each = merge([for pool in [
+    module.nuremberg_pool,
+    module.helsinki_pool,
+  ] : { "${pool.prefix}" = pool }]...)
+  source = "github.com/miran248/terraform-talos-modules//modules/hcloud-apply?ref=v2.0.0"
+
+  cluster = module.talos_cluster
+  pool    = each.value
+}
+
+module "talos_apply" {
+  source = "github.com/miran248/terraform-talos-modules//modules/talos-apply?ref=v2.0.0"
+
+  cluster = module.talos_cluster
+}
+
+resource "google_dns_record_set" "talos_ipv6" {
+  name         = "k.${data.google_dns_managed_zone.this.dns_name}"
+  managed_zone = data.google_dns_managed_zone.this.name
+  type         = "AAAA"
+  ttl          = 300
+
+  rrdatas = module.talos_cluster.public_ips6.control_planes
+}
+
+# outputs
+output "talos_config" {
+  value     = module.talos_cluster.talos_config
+  sensitive = true
+}
